fad7dabf33df1213479175daed4e2734e4f1b6e6
Dion Buckley <dion-b@hotmail.com>
2019-11-06 09:55:42 +0000

    Added pairing project for starter kit and dual purpose gitignore

---
+++ Assets/Scripts/DecimalToAscii.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecimalToAscii
+{
+    public static string convert(int[] t_input)
+    {
+        string resultString = null;
+        for (int i = 0; i < t_input.Length; i++)
+        {
+            resultString += System.Convert.ToChar(t_input[i]);
+        }
+
+        return resultString;
+    }
+}
---
+++ Assets/Tests 1/DecimalToAsciiTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class DecimalToAsciiTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void DecimalToAsciiMostSimplePasses()
+        {
+            int[] input = { };
+            string expected = null;
+            string result = DecimalToAscii.convert(input);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void DecimalToAsciiSingleInt()
+        {
+            int[] input = { 32 };
+            string expected = " ";
+            string result = DecimalToAscii.convert(input);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void DecimalToAsciiDoubleInt()
+        {
+            int[] input = { 32,33 };
+            string expected = " !";
+            string result = DecimalToAscii.convert(input);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
