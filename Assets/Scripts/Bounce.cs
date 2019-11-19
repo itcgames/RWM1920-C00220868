ce4522ce45a406295d2c360dbce71d5238f93106
Dion Buckley <dion-b@hotmail.com>
2019-11-19 14:58:27 +0000

    Debugs for 2D collision and check for top surface collision and bounce code reached

--- Assets/Scripts/Bounce.cs
+++ Assets/Scripts/Bounce.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class Bounce : MonoBehaviour
 {
-    void OnCollisionEnter(Collision col)
+    public float bounceAmount = 10;
+    private Vector2 velocity;
+
+    void OnCollisionEnter2D(Collision2D col)
     {
+        Debug.Log("Collision?");
+
         // something like if the collision is from the top side
         if (col.gameObject.GetComponent<Transform>().position.y >= this.gameObject.GetComponent<Transform>().position.y)
         {
+            Debug.Log("Above?");
+
             bounce(col.gameObject);
         }
     }
     public void bounce(GameObject other)
     {
-        // bounce maths
+        velocity = other.GetComponent<Rigidbody2D>().velocity;
+        velocity.y = 0;
+        other.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, bounceAmount));
+        Debug.Log("Bounce?");
     }
 }
