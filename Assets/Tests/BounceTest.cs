d703bcf3901005a9e4bb6e9fe68225dc19a1ad5d
Dion Buckley <dion-b@hotmail.com>
2020-01-23 10:46:55 +0000

    Finally finished Bounce Play Mode Test

--- Assets/Tests/BounceTest.cs
+++ Assets/Tests/BounceTest.cs
@@ -8,25 +8,16 @@ namespace Tests
 {
     public class BounceTest
     {
-        // A Test behaves as an ordinary method
-        [Test]
-        public void BounceTestSimplePasses()
-        {
-            //Bounce bounce = new Bounce();
-            //bounce.bounce(new GameObject());
-            Assert.AreEqual(1, 1);
-        }
-
         // A UnityTest behaves like a coroutine in Play Mode.
         [UnityTest]
         public IEnumerator BounceTestWithEnumeratorPasses()
         {
-            // Use the Assert class to test conditions.
-            // Use yield to skip a frame.
-            GameObject trampoline = MonoBehaviour.Instantiate(Resources.Load<GameObject>("./Prefabs/Trampoline"));
-            Vector3 initialPos = trampoline.transform.position;
+            GameObject trampoline = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/TrampolineWithTear"));
+            GameObject testCube = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/TestCube"));
+            trampoline.GetComponent<Bounce>().bounceAmount = 1000;
+            Vector3 initialPos = testCube.transform.position;
             yield return new WaitForSeconds(1);
-            Vector3 endPos = trampoline.transform.position;
+            Vector3 endPos = testCube.transform.position;
             Assert.Greater(endPos.y, initialPos.y);
         }
     }
